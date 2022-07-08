a15d841c440fedf20a734741a3f7fb9a2bcfab97
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-08 23:57:16 +0530

    Update sizeof.cpp

--- basic/sizeof.cpp
+++ basic/sizeof.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "length of char is:"<<sizeof(char)<<endl;
-    cout << "length of int is:"<<sizeof(int)<<endl;
-    cout << "length of float is:"<<sizeof(double)<<endl;
-    cout << "length of wchar is:"<<sizeof(wchar_t)<<endl;
+    cout << "length of char is:"<<sizeof(char)<<endl;                       //1
+    cout << "length of int is:"<<sizeof(int)<<endl;                         //4
+    cout << "length of double is:"<<sizeof(double)<<endl;                   //8
+    cout << "length of wchar is:"<<sizeof(wchar_t)<<endl;                   //4
     return 0;
 }
