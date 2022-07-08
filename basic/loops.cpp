b088657b4044e9064858182f424cd3eb3aa24a0d
Adarsh Masekar <108608608+AdarshMasekar@users.noreply.github.com>
2022-07-09 00:57:28 +0530

    Create loops.cpp

---
+++ basic/loops.cpp
@@ -0,0 +1,41 @@
+//for loop
+#include <iostream>
+using namespace std;      //important don't remove this line or else you will face errors 
+int main()
+{
+  int i,n=5;
+  for(i=0;i<n;i++){
+    cout<<"good work adarsh\";
+  }
+  return 0;
+}
+
+
+/* while loop
+#include <iostream>
+using namespace std;
+int main()
+{
+int i=0,n=5;
+while(i<n){
+cout<<"good work adarsh\n";
+i++;
+}
+return 0;
+}
+
+*/
+
+/* do while loop
+#include <iostream>
+using namespace std;
+int main()
+{
+int i=0,n=5;
+do{
+cout<<"good work adarsh\n";
+i++;
+}while(i<n);
+return 0;
+}
+*/
